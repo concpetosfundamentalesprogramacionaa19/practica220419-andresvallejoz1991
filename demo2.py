61586c6196134aa87091c39208a8919bbf5df356
andresvallejoz1991 <andresvallejoz@hotmail.com>
2019-04-22 16:16:53 -0500

    Ejercicios Python

---
+++ demo2.py
@@ -0,0 +1,14 @@
+"""
+Archivo : demo.py
+Ejemplo de lenguaje Python
+"""
+import sys
+
+nombrearchivo = sys.argv[0]
+valor1 = sys.argv[1]
+valor2 = sys.argv[2]
+suma = int(valor1)+int(valor2) #Se realiza la suma de variables
+
+
+
+print("La suma es :    %d"  %   suma)
---
+++ demo3.py
@@ -0,0 +1,19 @@
+"""
+Archivo : demo3.py
+Ejemplo de lenguaje Python
+Andrés Vallejo Z
+"""
+
+
+print ("Ingrese el valor 1")
+valor1 = input()
+print ("Ingrese el valor 2")
+valor2 = input()
+suma = int (valor1)+int(valor2)# Suma de los valores
+multiplicacion = int (valor1)* int (valor2)
+
+
+
+
+
+print("La suma es :  %d\tLa multiplicacion es: %d"  %   (suma, multiplicacion))
